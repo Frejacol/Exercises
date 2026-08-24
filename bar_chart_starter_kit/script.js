572b89e48995ffe43052203c9c2cb62a13fe8be9
Freja Colsted <frco0001@stud.ek.dk>
2026-08-24 17:20:25 +0200

    Add initial files for bar chart project: create index.html, script.js, and style.css; configure live server settings

---
+++ bar_chart_starter_kit/script.js
@@ -0,0 +1,25 @@
+import { generateRandomNumber } from "../../utils_lib/utils_lib.js";
+
+const list = document.querySelector("ul");
+const valueArr = [];
+
+setInterval(generateColumns, 500);
+
+function generateColumns() {
+  let height = generateRandomNumber(100);
+  valueArr.push(height);
+
+  const li = document.createElement("li");
+  li.style.setProperty("--height", height);
+  list.appendChild(li);
+
+  if (valueArr.length > 20) {
+    valueArr.shift();
+  }
+
+  if (list.children.length > 20) {
+    list.removeChild(list.firstElementChild);
+  }
+
+  console.log("Value Array", valueArr);
+}
