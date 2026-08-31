98deac1975ea71605b372d7ce76caef1d8e431f4
Freja Colsted <frco0001@stud.ek.dk>
2026-08-31 13:24:58 +0200

    Add initial files for capitalization feature: create capitalization3.html and capitalization3.js

---
+++ capitalization/capitalization3.js
@@ -0,0 +1,4 @@
+const capitalizedName = "freja";
+const thirdLetter = capitalizedName.substring(2, 3);
+
+console.log(`${capitalizedName.substring(0, 2)}${thirdLetter.toUpperCase()}${capitalizedName.substring(3)}`);
