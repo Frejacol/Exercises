48f500f55935e0e77dcc0260fbb75806efab0dd7
Freja Colsted <frco0001@stud.ek.dk>
2026-08-31 13:55:54 +0200

    Reorganize name extraction logs for clarity and remove duplicate console outputs

--- nameparts/nameparts.js
+++ nameparts/nameparts.js
@@ -1,9 +1,14 @@
 const fullName = "Freja Flinck Colsted";
-const longName = "Albus Percival Wulfric Brian Dumbledore";
 
 const firstName = `${fullName.substring(0, fullName.indexOf(" "))}`;
 const middleName = `${fullName.substring(fullName.indexOf(" ") + 1, fullName.lastIndexOf(" "))}`;
 const lastName = `${fullName.substring(fullName.lastIndexOf(" ") + 1)}`;
+
+console.log(`First name: ${firstName}`);
+console.log(`Middle name: ${middleName}`);
+console.log(`Last name: ${lastName}`);
+
+const longName = "Albus Percival Wulfric Brian Dumbledore";
 
 const longFirstName = `${longName.substring(0, longName.indexOf(" "))}`;
 const longMiddleName = `${longName.substring(longName.indexOf(" ") + 1, longName.lastIndexOf(" "))}`;
@@ -12,7 +17,3 @@ const longLastName = `${longName.substring(longName.lastIndexOf(" ") + 1)}`;
 console.log(`First name: ${longFirstName}`);
 console.log(`Middle name: ${longMiddleName}`);
 console.log(`Last name: ${longLastName}`);
-
-console.log(`First name: ${firstName}`);
-console.log(`Middle name: ${middleName}`);
-console.log(`Last name: ${lastName}`);
