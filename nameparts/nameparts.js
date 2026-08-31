d59b559642601c6d65b30573ae248a478b8c6eac
Freja Colsted <frco0001@stud.ek.dk>
2026-08-31 13:10:49 +0200

    Add initial files for nameparts feature: create nameparts.html and nameparts.js

---
+++ nameparts/nameparts.js
@@ -0,0 +1,10 @@
+const fullName = "Freja Flinck Colsted";
+const longName = "Albus Percival Wulfric Brian Dumbledore";
+
+console.log(`First name: ${fullName.substring(0, 5)}`);
+console.log(`Middle name: ${fullName.substring(6, 12)}`);
+console.log(`Last name: ${fullName.substring(13)}`);
+
+console.log(`First name: ${longName.substring(0, 5)}`);
+console.log(`Middle name: ${longName.substring(6, 28)}`);
+console.log(`Last name: ${longName.substring(29)}`);
